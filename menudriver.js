8bd6c5d24d1b31ef3d750ec6714e9251b9e5a1d3
Derek Norrbom <dnorrbom@hakkasan.com>
2020-08-27 20:31:33 -0700

    Create menudriver.js

---
+++ menudriver.js
@@ -0,0 +1,40 @@
+(function($) {
+
+    $.fn.menuedriver = function(options) {
+
+        // Default options
+        var settings = $.extend({
+            venue: 'yauatcha-riyadh',
+            domain: 'https://menus.hakkasangroup.com/'
+        }, options);
+
+        return this.each(function() {
+
+            // Variables
+            var wrapper = $(this);
+
+            $.ajax({
+                url: settings.domain + settings.venue,
+                type: 'GET',
+                success: function(data) {
+                    var
+                        styles = $('<div>').append(data).find('style'),
+                        scripts = $('<div>').append(data).find('script'),
+                        menu = $('<div>').append(data).find('.menu-content');
+
+                    $(scripts).each(function() {
+                        if (this.src != null) {
+                            this.parentNode.removeChild(this);
+                        }
+                    });
+
+                    var fullmenu = $.merge(menu, styles, scripts);
+                    wrapper.html(fullmenu);
+                }
+            });
+
+        });
+
+    };
+
+}(jQuery));
